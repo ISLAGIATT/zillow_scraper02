5d2075e1930f940de43794d2172f1102fc0a1e35
ISLAGIATT <133043444+ISLAGIATT@users.noreply.github.com>
2024-05-14 13:39:53 -0700

    test maui up to 350k

--- main.py
+++ main.py
@@ -65,15 +65,15 @@ def scrape_big_island_zillow():
 
 
 def scrape_maui_zillow():
-    url = ("https://www.zillow.com/maui-county-hi/?searchQueryState=%7B%22isMapVisible%22%3Atrue%2C%22mapBounds%22%3A"
-           "%7B%22north%22%3A21.38728402791739%2C%22south%22%3A20.345325891144547%2C%22east%22%3A-155.84329175585935"
-           "%2C%22west%22%3A-157.4500422441406%7D%2C%22usersSearchTerm%22%3A%22Maui%2C%20HI%22%2C%22filterState%22%3A"
-           "%7B%22sort%22%3A%7B%22value%22%3A%22globalrelevanceex%22%7D%2C%22tow%22%3A%7B%22value%22%3Afalse%7D%2C"
-           "%22mf%22%3A%7B%22value%22%3Afalse%7D%2C%22land%22%3A%7B%22value%22%3Afalse%7D%2C%22ah%22%3A%7B%22value%22"
-           "%3Atrue%7D%2C%22apa%22%3A%7B%22value%22%3Afalse%7D%2C%22manu%22%3A%7B%22value%22%3Afalse%7D%2C%22price%22"
-           "%3A%7B%22max%22%3A300000%7D%2C%22mp%22%3A%7B%22max%22%3A1558%7D%7D%2C%22isListVisible%22%3Atrue%2C"
-           "%22regionSelection%22%3A%5B%7B%22regionId%22%3A250%2C%22regionType%22%3A4%7D%5D%2C%22pagination%22%3A%7B"
-           "%7D%7D")
+    url = ("https://www.zillow.com/maui-county-hi/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22isMapVisible%22"
+           "%3Atrue%2C%22mapBounds%22%3A%7B%22west%22%3A-157.4500422441406%2C%22east%22%3A-155.84329175585935%2C"
+           "%22south%22%3A20.345325891144544%2C%22north%22%3A21.387284027917385%7D%2C%22usersSearchTerm%22%3A%22Maui"
+           "%20County%2C%20HI%22%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A250%2C%22regionType%22%3A4%7D%5D%2C"
+           "%22filterState%22%3A%7B%22sort%22%3A%7B%22value%22%3A%22globalrelevanceex%22%7D%2C%22price%22%3A%7B%22min"
+           "%22%3A0%2C%22max%22%3A350000%7D%2C%22mp%22%3A%7B%22min%22%3A0%2C%22max%22%3A1815%7D%2C%22tow%22%3A%7B"
+           "%22value%22%3Afalse%7D%2C%22mf%22%3A%7B%22value%22%3Afalse%7D%2C%22land%22%3A%7B%22value%22%3Afalse%7D%2C"
+           "%22ah%22%3A%7B%22value%22%3Atrue%7D%2C%22apa%22%3A%7B%22value%22%3Afalse%7D%2C%22manu%22%3A%7B%22value%22"
+           "%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D")
     with httpx.Client(http2=True, headers=BASE_HEADERS, follow_redirects=True) as client:
         resp = client.get(url)
     sel = Selector(text=resp.text)
